db3d958c91e00b8d08241bac5e2a13b8d0165a8d
Swapnil <swapnil.kumar@entropiktech.com>
2022-12-15 15:59:54 +0530

    made it more readable

--- DecodeCx1.py
+++ DecodeCx1.py
@@ -5,115 +5,113 @@ from string import Template
 
 
 tester_details=[]
+n1=9  # No. of callings for all blocks
+n2=2  # No. of testers giving test
+n3=8  # No. of block_insights
 insights=''  # To store the block_insights response
 study_id='4def2733-f775-462b-a65f-2d20469064fe'
 base_url='https://in.dev.apicx.getdecode.io/v1/'
 login_url='https://app.dev.getdecode.io/authentication/login'
 tester_url='tester'
-study_url=['test/blocks?study_id=$study_id&block_id=$block_id&tester_id=$tester','tester_response']
-study_url_2=Template(study_url[0])
+get_resp_url='test/blocks?study_id=$study_id&block_id=$block_id&tester_id=$tester'
+get_resp_url_2=Template(get_resp_url)
+post_resp_url='tester_response'
 insights_url='blocks_insights'
 
-request_json={'username': "bantupalli.sriharsha@entropiktech.com", 'password': "Harsha@10150", 'workspace': "beintern"}
-jsondata=json.dumps(request_json)
-response=requests.post(login_url, data=jsondata)
-assert response.status_code==200, 'Error'
-c=response.content
-c=json.loads(c.decode('utf-8'))
-access_token=c['data']['access_token']
-id_token=c['data']['id_token']
-headers1= {'authorization': access_token, 'id_token': id_token, 'workspace_id': '01GG7AW75CHDNZ2BNYKZS9ZHMF'}
+def generate_headers():
+#To generater authorization id and id token***********************************************************************************************************
+    request_json={'username': "bantupalli.sriharsha@entropiktech.com", 'password': "Harsha@10150", 'workspace': "beintern"}
+    jsondata=json.dumps(request_json)
+    response=requests.post(login_url, data=jsondata)
+    assert response.status_code==200, 'Error'
+    c=response.content
+    c=json.loads(c.decode('utf-8'))
+    access_token=c['data']['access_token']
+    id_token=c['data']['id_token']
+    return {'authorization': access_token, 'id_token': id_token, 'workspace_id': '01GG7AW75CHDNZ2BNYKZS9ZHMF'}
 
 
 
-#To generate tester id using post method******************************************************************************************************
-def call_tester_url(a):
-
+def generate_testers(url):
+#To generate tester id using post method*********************************************************************************************************
     global tester_details
-    global block_id
-    global option_ids
     global tester
     
-    url=a
+    url=url
 
-    if(url==base_url+tester_url):
-        request_json={"study_id":study_id,"workspace_id":headers1['workspace_id'],"preview":True,"variables":{}}
-        jsondata=json.dumps(request_json)
-        response=requests.post(url, headers=headers1, data=jsondata)
-        assert response.status_code==200, 'Error'
-        c=response.content
-        c=json.loads(c.decode('utf-8'))
-        tester=c['data']['tester']['tester_id']
-        tester_details.append(tester)
-        print()
+    request_json={"study_id":study_id,"workspace_id":headers1['workspace_id'],"preview":True,"variables":{}}
+    jsondata=json.dumps(request_json)
+    response=requests.post(url, headers=headers1, data=jsondata)
+    assert response.status_code==200, 'Error'
+    c=response.content
+    c=json.loads(c.decode('utf-8'))
+    tester=c['data']['tester']['tester_id']
+    tester_details.append(tester)
+    print()
 
     
 
+
+def generate_block_options(url):
 #To generate block_id and option_id using get method**********************************************************************************************
-def call_study_test_resp_url(a):
-
     global block_id
     global option_ids
-    global tester
-    global blocker
-    url=a
+    global block_id_list
+    url=url
 
-    if(url==base_url+study_url[0]):
-        response=requests.get(url, headers=headers1)
-        assert response.status_code==200, 'Error'
-        c=response.content
-        c=json.loads(c.decode('utf-8'))
-        block_id=c['data']['block']['block_id']
+    response=requests.get(url, headers=headers1)
+    assert response.status_code==200, 'Error'
+    c=response.content
+    c=json.loads(c.decode('utf-8'))
+    block_id=c['data']['block']['block_id']
 
-        bt=c['data']['block']['block_type']
-        if(bt =="SHORT_ANSWER" or bt=="PARAGRAPH"):
-            option_ids=['Survey was good', 'product was good', 'survey was decent', 'product is not that good']
-            blocker.append(block_id)
+    bt=c['data']['block']['block_type']
+    if(bt =="SHORT_ANSWER" or bt=="PARAGRAPH"):
+        option_ids=['Survey was good', 'product was good', 'survey was decent', 'product is not that good']
+        block_id_list.append(block_id)
 
-        elif(bt =="LIKE_DISLIKE" or bt=="LINEAR_SCALE" or bt=="SMILEY_RATING" or bt=="STAR_RATING" ):
-            option_ids=c['data']['block']['block_properties']['scale']
-            blocker.append(block_id)
+    elif(bt =="LIKE_DISLIKE" or bt=="LINEAR_SCALE" or bt=="SMILEY_RATING" or bt=="STAR_RATING" ):
+        option_ids=c['data']['block']['block_properties']['scale']
+        block_id_list.append(block_id)
 
-        elif(bt=="CHECKBOX" or bt=="MCQ_BLOCK" or bt=="DROPDOWN"):
-            l=len(c['data']['block']['block_properties']['options'])
-            for i in range(l):
-                option_ids.append(c['data']['block']['block_properties']['options'][i]['option_id'])
-            
-            blocker.append(block_id)
+    elif(bt=="CHECKBOX" or bt=="MCQ_BLOCK" or bt=="DROPDOWN"):
+        l=len(c['data']['block']['block_properties']['options'])
+        for i in range(l):
+            option_ids.append(c['data']['block']['block_properties']['options'][i]['option_id'])
+        
+        block_id_list.append(block_id)
 
-        else: #(For thankyou page)
-            block_id="14d2b236-2727-46b6-b7d2-bfc5029109d2"
-            option_ids=[]
+    else: #(For thankyou page)
+        block_id="14d2b236-2727-46b6-b7d2-bfc5029109d2"
+        option_ids=[]
 
 
-##Calling tester_url by passing block_id and option_id as response using post method
 
-    if(url==base_url+study_url[1]):
+def pass_response(url):
+##Calling tester_url by passing block_id and option_id as response using post method*************************************************************
+    url=url
 
-        if(len(option_ids)>0):
-            r=random.randint(0, len(option_ids)-1)
-            request_json={"block_id":block_id,"study_id":study_id,"tester_id":tester,"response":[option_ids[r]]}
-        else:
-            request_json={"block_id":block_id,"study_id":study_id,"tester_id":tester,"response":option_ids}
+    if(len(option_ids)>0):
+        r=random.randint(0, len(option_ids)-1)
+        request_json={"block_id":block_id,"study_id":study_id,"tester_id":tester,"response":[option_ids[r]]}
+    else:
+        request_json={"block_id":block_id,"study_id":study_id,"tester_id":tester,"response":option_ids}
 
-        jsondata=json.dumps(request_json)
+    jsondata=json.dumps(request_json)
 
-        response=requests.post(url, headers=headers1, data=jsondata)
-        assert response.status_code==200, 'Error'
+    response=requests.post(url, headers=headers1, data=jsondata)
+    assert response.status_code==200, 'Error'
 
 
+
+def call_insight_url(url):
 ##calling block_insights urls************************************************************************************************************************
-def call_insight_url(a):
-
-    global block_id
     global insights
-    global blocker
-    global tester_details
 
-    url=a
+    url=url
 
-    for i in range(8):
-        request_json={"study_id":study_id,"block_id":blocker[i],"tester_ids":[]}
+    for i in range(n3):
+        request_json={"study_id":study_id,"block_id":block_id_list[i],"tester_ids":[]}
         jsondata=json.dumps(request_json)
         response=requests.post(url, headers=headers1, data=jsondata)
         assert response.status_code==200, 'Error'
@@ -132,41 +130,35 @@ def call_insight_url(a):
             print(i+1,') Total no. of responses are not same as the total no. of tester ids for', insights['data']['block_name'], 
             ',Total responses: ', insights['data']['insights']['finished']['total'])
 
-#Function to call all the methods***********************************************************************************************************************
+
 def calling_methods():
-
-    global block_id
-    global tester
-
-    call_tester_url(base_url+tester_url)
-    b=0
-
-    for _ in range(18):
-        study_url[0]=study_url_2.substitute(study_id=study_id, block_id=block_id, tester=tester)
-        call_study_test_resp_url(base_url+study_url[b])
-        b=b+1
-        if(b>1):
-            b=0   
+#Function to call all the methods***********************************************************************************************************************
+    generate_testers(base_url+tester_url)
 
 
-<<<<<<< HEAD
-=======
-    for i in range(6):
-        call_insight_url(base_url+end_points3[i])
+    for _ in range(n1):
+        get_resp_url=get_resp_url_2.substitute(study_id=study_id, block_id=block_id, tester=tester)
+        generate_block_options(base_url+get_resp_url)
 
-#Using the for loop to perform the test for n no. of testers
->>>>>>> 53e1a91ccb974336d3309bc05e1a79e338a3a760
-for  t in range(2):
-
-    block_id='99d22693-ed4f-4474-b42a-ffa89a519b49'
-    blocker=[]
-    option_ids=[]
-    tester=''
-
-    print(f'Test no. {t+1} started.........')
-    calling_methods()
-    print(f'Test no. {t+1} done')
-    print()
+        pass_response(base_url+post_resp_url) 
 
 
-call_insight_url(base_url+insights_url)
+if __name__=='__main__':
+
+    headers1=generate_headers()
+
+
+    for  t in range(n2):
+
+        block_id='99d22693-ed4f-4474-b42a-ffa89a519b49'
+        block_id_list=[]
+        option_ids=[]
+        tester=''
+
+        print(f'Test no. {t+1} started.........')
+        calling_methods()
+        print(f'Test no. {t+1} done')
+        print()
+
+
+    call_insight_url(base_url+insights_url)
